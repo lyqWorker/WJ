78f188d320fc7b8b4dcd9a0141fb6fc9b7740392
陆玉强 <770291651@qq.com>
2018-10-24 23:16:16 +0800

    login

---
+++ 2-Client/WJSite/Controllers/CommonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WJSite.Controllers
+{
+    [RoutePrefix("api/common")]
+    public class CommonController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("message")]
+        public HttpResponseMessage GetMessage()
+        {
+            var resp = new HttpResponseMessage();
+            resp.Content = new StringContent("OK");
+            return resp;
+        }
+    }
+}
---
+++ 2-Client/WJSite/Resources/custom/js/login.js
@@ -0,0 +1,31 @@
+﻿var login = function () {
+    var username, pwd, code;
+
+
+    var checkNetwork = function () {
+        $.ajax({
+            url: '/api/common/message',
+            type: 'get',
+            dataType: 'text',
+            success: function (data) {
+                console.log(data);
+            },
+            error: function () {
+
+            }
+        });
+    };
+
+
+
+
+
+
+
+
+    return {
+        init: function () {
+            checkNetwork();
+        }()
+    };
+}()
