d4766522adab737b5e01f1e2982d77fdff869020
陆玉强 <770291651@qq.com>
2019-01-03 08:46:32 +0800

    新版本19.01.03

--- 3-TestProject/TestMethod/Program.cs
+++ 3-TestProject/TestMethod/Program.cs
@@ -15,38 +15,6 @@ namespace TestMethod
     {
         static void Main(string[] args)
         {
-            /**RSA加密测试,RSA中的密钥对通过SSL工具生成，生成命令如下： 
-           * 1 生成RSA私钥： 
-           * openssl genrsa -out rsa_private_key.pem 1024 
-           *2 生成RSA公钥 
-           * openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem 
-           * 
-           * 3 将RSA私钥转换成PKCS8格式 
-           * openssl pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -nocrypt -out rsa_pub_pk8.pem 
-           * 
-           * 直接打开rsa_private_key.pem和rsa_pub_pk8.pem文件就可以获取密钥对内容，获取密钥对内容组成字符串时，注意将换行符删除 
-           * */
-            //rsa_pub_pk8.pem内容
-           
-            //加密字符串  
-            string data = "yangyoushan";
-            string publickey = "";
-            string privatekey = "";
-            using (StreamReader reader = new StreamReader("publicKey.txt"))
-            {
-                publickey = reader.ReadToEnd();
-            }
-            using (StreamReader reader = new StreamReader("privateKey.txt"))
-            {
-                privatekey = reader.ReadToEnd();
-            }
-
-            Console.WriteLine("加密前字符串内容：" + data);
-            //加密  
-            
-            string encrypteddata = RSAUtils.EncryptData(data, publickey, "UTF-8");
-            Console.WriteLine("加密后的字符串为：" + encrypteddata);
-            Console.WriteLine("解密后的字符串内容：" + RSAUtils.DecryptData(encrypteddata, privatekey, "UTF-8"));
            
 
             Console.ReadKey();
